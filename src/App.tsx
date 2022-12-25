7691ed7c08bbe94bd7bb50d18d0f125f23ba39b8
Mayank Ashok <akmayank@amazon.com>
2022-12-24 19:29:26 -0800

    add input field and Table with sort and select

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Grid } from "@mui/material";
+import Box from "@mui/material/Box/Box";
+import TextField from "@mui/material/TextField/TextField";
+import ButtonAppBar from "./components/ButtonAppBar";
+import TableSortSelect from "./components/TableSortSelect";
+
 function App() {
-  return <p>Initial App</p>;
+  return (
+    <div>
+      <ButtonAppBar />
+      <h1>Grocery List</h1>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={4}>
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Box sx={{ m: 10 }}>
+        <TableSortSelect />
+      </Box>
+    </div>
+  );
 }
 
 export default App;
